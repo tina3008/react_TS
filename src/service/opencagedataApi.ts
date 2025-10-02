2e3776ea65ddc997ef076341b13d5f7002146122
tina3008 <kicja_3@ukr.net>
2025-10-02 16:57:40 +0300

    geo type

--- src/service/opencagedataApi.ts
+++ src/service/opencagedataApi.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export const getUserInfo = async ({ latitude, longitude }) => {
+interface getUserInfoProps {
+  latitude: number;
+  longitude: number;
+};
+
+export const getUserInfo = async ({ latitude, longitude }: getUserInfoProps) => {
   const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
   const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}`;
   const { data } = await axios.get(urlPosition, {
